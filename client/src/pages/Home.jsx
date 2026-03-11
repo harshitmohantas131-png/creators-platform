05b9628b1b58a63be03df20e75678da249822d90
Harshit Mohanta <harshit.mohanta.s.131@kalvium.community>
2026-03-11 10:12:51 +0530

    Configure CORS and Vite Proxy for frontend-backend communication

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import ConnectionTest from "../components/common/ConnectionTest";
 
 const Home = () => {
   return (
     <div style={{ textAlign: "center", padding: "3rem" }}>
       <h1>Welcome to CreatorHub</h1>
       <p>A platform for creators to share their work.</p>
+
+      <ConnectionTest />
 
       <div style={{ marginTop: "2rem" }}>
         <Link to="/register" style={{ marginRight: "1rem" }}>
