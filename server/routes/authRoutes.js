8f0abed996ea7afdf7a26126698dd6abbaf4063e
Harshit Mohanta <harshit.mohanta.s.131@kalvium.community>
2026-04-22 11:05:33 +0530

    Add Docker Compose setup for full MERN stack with Dockerized backend and frontend

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -3,7 +3,6 @@ import { loginUser } from '../controllers/authController.js';
 import { registerUser } from '../controllers/authController.js';
 const router = express.Router();
 
-
 // Registration route
 router.post("/register", registerUser);
 // Login route
